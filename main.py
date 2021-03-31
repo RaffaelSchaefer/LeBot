63da75d8e90f91b67d72bd692a53370683146a30
Raffael Schaefer <raffael.elias.schaefer@gmail.com>
2021-03-31 09:15:34 +0000

    Added console output for bot commands

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ async def on_message(message):
   if msg.startswith("$Neu"):
     fragen_message = msg.split("$Neu ",1)[1]
     update_fragen(fragen_message)
+    print("Neue Frage hinzugefügt: "+ fragen_message)
     await message.channel.send("Neue Frage hinzugefügt: "+ fragen_message)
   
   if msg.startswith("$Ent"):
@@ -42,13 +43,16 @@ async def on_message(message):
       index = int(msg.split("$Ent",1)[1])
       delete_fragen(index)
       fragen = db["fragen"]
+    print('{0.author.name} löschte eine Frage'.format(message))
     await message.channel.send(fragen)
 
   if msg.startswith("$List"):
     fragen = db["fragen"]
+    print('{0.author.name} möchte alle Fragen wissen'.format(message))
     await message.channel.send(fragen)
 
   if msg.startswith("$Frage"):
+    print('{0.author.name} moechte eine Frage haben'.format(message))
     await message.channel.send(random.choice(db["fragen"]))
 
 keep_alive()
