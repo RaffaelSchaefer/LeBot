10d710117bc10695803e7e1894ce5e2abc6318af
Raffael Schaefer <raffael.elias.schaefer@gmail.com>
2021-04-01 13:57:53 +0000

    Added Dare Database

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
-def update_fragen(fragen_message,dbKey):
+def update_db(fragen_message,dbKey):
   if dbKey in db.keys():
     fragen = db[dbKey]
     fragen.append(fragen_message)
@@ -14,7 +14,7 @@ def update_fragen(fragen_message,dbKey):
   else: 
     db[dbKey] = [fragen_message]
 
-def delete_fragen(index,dbKey):
+def delete_db(index,dbKey):
   fragen = db[dbKey]
   if len(fragen) > index:
     del fragen[index]
@@ -36,28 +36,52 @@ async def on_message(message):
 
   msg = message.content
   dbFragen = "Fragen_"+str(message.guild.id)
+  dbPflicht = "Pflicht_"+str(message.guild.id)
   
   if msg.startswith("$newQ"):
     fragen_message = msg.split("$newQ ",1)[1]
-    update_fragen(fragen_message,dbFragen)
+    update_db(fragen_message,dbFragen)
     print("Neue Frage hinzugefügt: "+ fragen_message)
     await message.channel.send("Neue Frage hinzugefügt: "+ fragen_message)
+
+  if msg.startswith("$newD"):
+    pflicht_message = msg.split("$newD ",1)[1]
+    update_db(pflicht_message,dbPflicht)
+    print("Neue Pflicht hinzugefügt: "+ pflicht_message)
+    await message.channel.send("Neue Pflicht hinzugefügt: "+ pflicht_message)
   
   if msg.startswith("$delQ"):
     fragen = []
     if dbFragen in db.keys():
       index = int(msg.split("$delQ",1)[1])
-      delete_fragen(index,dbFragen)
+      delete_db(index,dbFragen)
       fragen = db[dbFragen]
     print('{0.author.name} loeschte eine Frage'.format(message))
     for i in range(0, len(db[dbFragen])):
       await message.channel.send(create_list(dbFragen,i))
     print("Listen Ausgabe fertig")
 
+  if msg.startswith("$delD"):
+    pflicht = []
+    if dbPflicht in db.keys():
+      index = int(msg.split("$delQ",1)[1])
+      delete_db(index,dbPflicht)
+      pflicht = db[dbPflicht]
+    print('{0.author.name} loeschte eine Frage'.format(message))
+    for i in range(0, len(db[dbPflicht])):
+      await message.channel.send(create_list(dbPflicht,i))
+    print("Listen Ausgabe fertig")
+
   if msg.startswith("$listQ"):
     print('{0.author.name} moechte alle Fragen wissen'.format(message))
     for i in range(0, len(db[dbFragen])):
       await message.channel.send(create_list(dbFragen,i))
+    print("Listen Ausgabe fertig")
+  
+  if msg.startswith("$listD"):
+    print('{0.author.name} moechte alle Fragen wissen'.format(message))
+    for i in range(0, len(db[dbPflicht])):
+      await message.channel.send(create_list(dbPflicht,i))
     print("Listen Ausgabe fertig")
 
   if msg.startswith("$question"):
