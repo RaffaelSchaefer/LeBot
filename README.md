66b242a4e78849a83f0d91202e18794697133d53
Raffael Schaefer <>
2021-04-02 10:16:37 +0000

    Rewrote truth or drink and finished truth or dare

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # LeBot
 ![LeBot Logo](https://github.com/RaffaelSchaefer/LeBot/blob/master/Icons/Chat_small.png?raw=true)
 
-LeBot is a small Discord game bot
+LeBot is a small Discord game bot.
+It uses the new slash command system
 
 ## Games
 
 ### Games included
 
 - Truth or Drink
+- Truth or Dare
 
 ### Games planed
-
-- Truth or Dare (WIP)
 
 - Who is most likely
 
@@ -19,17 +19,5 @@ LeBot is a small Discord game bot
 
 |Command|Usage|
 |--|--|
-| /help | Shows all commands |
-| /newQ (question) | A new question is added to the question database |
-| /delQ (index) | The question at the index is deleted from the database |
-| /listQ | Lists all the questions in the database |
-| /getQ (index) | Shows the Question a the index point |
-| /newD (dare) | A new dare is added to the question database |
-| /delD (index) | The dare at the index is deleted from the database |
-| /listD | Lists all the dare in the database |
-| /getD (index) | Shows the Dare a the index point |
-| /question | Selects a random question from the database |
-| /dare | Selects a random dare from the database |
-| /info | Shows all the stats |
 | /TruthOrDrink | Outputs a truth or drink question |
 | /TruthOrDare | Starts a new round of Truth or Dare (incomplete) |
--- main.py
+++ main.py
@@ -3,19 +3,60 @@ import os
 import random
 from replit import db
 from keep_alive import keep_alive
-from discord_slash import SlashCommand
+from discord_slash import SlashCommand, SlashCommandOptionType, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
 
 client = discord.Client()
 slash = SlashCommand(client, sync_commands=True)
+
+##Functions
+
+##Startup
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
-@slash.slash(name="info",
-             description="Shows all the stats from the bot")
-async def info(ctx):
-  await ctx.send(content="Hello World!")
+##Options
+
+option_TruthOrDare = [
+  create_option(
+    name="mode",
+    description="choose an entry based on mode selection",
+    option_type=3,
+    required=True,
+    choices=[
+      create_choice(
+      name="Truth",
+      value="truth"
+    ),
+      create_choice(
+      name="Dare",
+      value="dare"
+    )
+    ]
+  )
+]
+
+##Commands
+
+@slash.slash(name="TruthOrDrink", description="Starts a new round of Truth or Drink")
+async def TruthOrDrink(ctx):
+  dbFragen = "Fragen_"+str(ctx.guild.id)
+  print('{0.author.name} wants to play a round Truth or Drink'.format(ctx))
+  await ctx.send(content=random.choice(db[dbFragen]))
+
+@slash.slash(name="TruthOrDare", description="Starts a new round of Truth or Dare",options=option_TruthOrDare)
+async def TruthOrDare(ctx,mode: str):
+  dbFragen = "Fragen_"+str(ctx.guild.id)
+  dbPflicht = "Pflicht_"+str(ctx.guild.id)
+  print('{0.author.name} wants to play a round Truth or Dare'.format(ctx))
+  if mode == "truth":
+    print('{0.author.name} choose truth'.format(ctx))
+    await ctx.send(content="Truth: "+random.choice(db[dbFragen]))
+  if mode == "dare":
+    print('{0.author.name} choose dare'.format(ctx))
+    await ctx.send(content="Dare: "+random.choice(db[dbPflicht]))
 
 #keep_alive()
 client.run(os.getenv('TOKEN'))
