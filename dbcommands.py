fe76d7701935d66efeba56a996946a6d4264fa77
Raffael Schaefer <>
2021-04-05 11:46:43 +0000

    wrote transfer_db

--- dbcommands.py
+++ dbcommands.py
@@ -18,3 +18,32 @@ def get_dbEntry(dbKey,index):
   out = str(index)+": "+str(db[dbKey][index])+"\n"
   print(out)
   return out
+
+def tranfer_db(currentServerID,targetServerID):
+  #Current Server Keys
+  currentDbFragen = "Fragen_"+str(currentServerID)
+  currentDbPflicht = "Pflicht_"+str(currentServerID)
+  currentDbMostlikely = "Mostlikely_"+str(currentServerID)
+  currentDbTopics = "Topics_"+str(currentServerID)
+  #Target Server Keys
+  targetDbFragen = "Fragen_"+str(targetServerID)
+  targetDbPflicht = "Pflicht_"+str(targetServerID)
+  targetDbMostlikely = "Mostlikely_"+str(targetServerID)
+  targetDbTopics = "Topics_"+str(targetServerID)
+  #Logic
+  if currentDbFragen in db.keys():
+    db[currentDbFragen] = db[currentDbFragen]+db[targetDbFragen]
+  else: 
+    db[currentDbFragen] = db[targetDbFragen]
+  if currentDbPflicht in db.keys():
+    db[currentDbPflicht] = db[currentDbPflicht]+db[targetDbPflicht]
+  else: 
+    db[currentDbPflicht] = db[targetDbPflicht]
+  if currentDbMostlikely in db.keys():
+    db[currentDbMostlikely] = db[currentDbMostlikely]+db[targetDbMostlikely]
+  else: 
+    db[currentDbMostlikely] = db[targetDbMostlikely]
+  if currentDbTopics in db.keys():
+    db[currentDbTopics] = db[currentDbTopics]+db[targetDbTopics]
+  else: 
+    db[currentDbTopics] = db[targetDbTopics]
