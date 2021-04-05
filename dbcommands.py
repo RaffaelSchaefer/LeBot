60d661eb2b198f934e9ff5ec023484c236feb7f2
Raffael Schaefer <>
2021-04-05 11:27:57 +0000

    created options.py

--- dbcommands.py
+++ dbcommands.py
@@ -1,5 +1,6 @@
 from replit import db
 
+##Add something to the db
 def update_dbEntry(fragen_message,dbKey):
   if dbKey in db.keys():
     fragen = db[dbKey]
@@ -8,12 +9,14 @@ def update_dbEntry(fragen_message,dbKey):
   else: 
     db[dbKey] = [fragen_message]
 
+##Delete something to the db
 def delete_dbEntry(index,dbKey):
   fragen = db[dbKey]
   if len(fragen) > index:
     del fragen[index]
     db[dbKey] = fragen
 
+##Get a db entry
 def get_dbEntry(dbKey,index):
   out = str(index)+": "+str(db[dbKey][index])+"\n"
   print(out)
